8a5993401423e3476376cb7117753e51a947a78b
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-09 15:38:32 +0200

    querying objects with hbernate

--- src/com/vladyslav/hibernate/demo/RetrivingStudents.java
+++ src/com/vladyslav/hibernate/demo/RetrivingStudents.java
@@ -1,12 +1,14 @@
 package com.vladyslav.hibernate.demo;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.vladyslav.demo.entity.Student;
 
-public class StudentDemo {
+public class RetrivingStudents {
 	
 	public static void main(String[] args) {
 		SessionFactory factory = new Configuration()
@@ -17,14 +19,23 @@ public class StudentDemo {
 	Session session = factory.getCurrentSession();
 	
 	try {
-		Student tempStudent = new Student("Vlad", "Haspor", "haspor@gmail.com");
-		Student tempStudent1 = new Student("Vladimir", "Kikobidze+", "kiko@gmail.com");
+	
 		session.beginTransaction();
-		session.save(tempStudent);
-		session.save(tempStudent1);
-		session.getTransaction().commit();;
-		System.out.println("Done!");
-	}finally {
+		
+		// query Students 
+		List<Student> list = session.createQuery("from Student").list();
+		
+		// display Students
+		list.forEach(lists -> System.out.println(lists));
+		
+		// query Students 
+		List<Student> list2 = session.createQuery("from Student s where s.firstName='Vlad'").list();
+		
+		// display Students
+		list2.forEach(lists -> System.out.println("Students who have first name - Vlad" + list));
+	
+		session.getTransaction().commit();
+	} finally {
 		session.close();
 	}
 	}
